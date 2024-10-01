8c7946ca27bf89fc4cfdc968a713ec62f3735ec0
Daniel Avila <arturo.avilar@my.unitec.edu.mx>
2024-09-30 21:27:48 -0600

    Create Funciones.cpp

---
+++ Funciones.cpp
@@ -0,0 +1,15 @@
+terminar de reparar el codigo.
+#include <iostream>
+#include <vector>
+
+using namespace std;
+//funcion prototipo
+void imprime(vector<int> v)
+
+int main()
+{
+	vector<int> v;
+	v.push_back(1);
+	v.push_back(2);
+	imprime(v);
+}
